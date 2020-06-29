77ac2667263136e750ae7b87caa7b8c4a7d5c07a
Róbert Reynisson <robertreynisson@Roberts-MacBook-Pro.local>
2020-06-29 21:32:07 +0000

    Adding java gradle project

---
+++ src/test/java/com/robertreynisson/accountmanager/AccountmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robertreynisson.accountmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
